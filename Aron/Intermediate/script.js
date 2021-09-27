37883d7688b9b0c64f9b08f57954bfc61ba61e25
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-27 14:10:49 +0200

    started intermediate

---
+++ Aron/Intermediate/script.js
@@ -0,0 +1,9 @@
+function gradePointAverage(grades){
+
+   console.log(grades[2][1]);
+
+}
+
+const grades = [["Martin",76], ["Thomas",85], ["Fatima",65], ["Maria",93],["David",81]];
+
+gradePointAverage(grades);
