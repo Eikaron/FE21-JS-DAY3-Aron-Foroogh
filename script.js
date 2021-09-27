810fa4a3860948b94dbe0b3257cc38b9838d829e
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-09-27 10:39:30 +0200

    basic

---
+++ script.js
@@ -0,0 +1,20 @@
+//Basic Exercise 1
+
+
+
+
+//Basic Exercise 2
+
+function highVal(array1) {
+
+ for (let i = 1; i< length.array1; i++)
+    console.log(array1[i]);
+}
+
+const myArray = [1, 5, -6, 3];
+highVal(myArray);
+
+//console.log(Math.max(...myArray));
+
+
+//Basic Exercise 3
